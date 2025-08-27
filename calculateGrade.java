07dabfaccb9edcbc3b24f3914ced71d01ee74c92
Ovaisgithub <Durrani_ovais@outlook.com>
2025-08-27 22:22:13 +0530

    grade calculation program
    Introduces calculateGrade.java, a program that prompts the user for a percentage and outputs the corresponding grade (A, B, C, D, or fail) based on predefined thresholds.

---
+++ calculateGrade.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class calculateGrade{
+    public static void calculateGrade(){
+        System.out.println("enter your percentage");
+        Scanner perinp = new Scanner(System.in);
+        int per = perinp.nextInt();
+        
+        if(per >= 90){
+            
+            System.out.println("your grade is A");
+        }
+        else if(per >= 80 && per < 90){
+            
+            System.out.println("your grade is B ");
+        }
+        else if(per >= 70 && per < 80){
+            
+            System.out.println("your grade is C " );
+        }
+        else if(per >= 60 && per < 70){
+            
+            System.out.println("your grade is D ");
+        }
+        else{
+            System.out.println("your grade is fail ");
+        }
+        perinp.close();
+
+    }
+    public static void main(String[] args){
+        calculateGrade();
+    }
+
+}
